eca5b6c86f2c8a63105c797bfb68744cc31a03a4
ckegel <57967583+CKegel@users.noreply.github.com>
2024-10-01 17:50:19 -0400

    Add edit button for those who can edit the schedule.

--- angular-app/src/app/crew-schedule/crew-schedule.component.ts
+++ angular-app/src/app/crew-schedule/crew-schedule.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { KeycloakService } from 'keycloak-angular';
 
 @Component({
   selector: 'app-crew-schedule',
   standalone: true,
-  imports: [],
+  imports: [ CommonModule ],
   templateUrl: './crew-schedule.component.html',
   styleUrl: './crew-schedule.component.css'
 })
-export class CrewScheduleComponent {
+export class CrewScheduleComponent implements OnInit {
+  public canModifySchedule = false;
 
+  constructor(private keycloakService: KeycloakService) {}
+  async ngOnInit(){
+    const roles = this.keycloakService.getUserRoles();
+    this.canModifySchedule = roles.includes('website-admin') || roles.includes('website-crew-scheduler');
+  }
 }
