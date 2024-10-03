55dc55660b3c031ed9ef2c1d26fd8fbded5eb47b
ckegel <57967583+CKegel@users.noreply.github.com>
2024-10-02 21:41:34 -0400

    Add event page controls.

--- angular-app/src/app/event-schedule/event-schedule.component.ts
+++ angular-app/src/app/event-schedule/event-schedule.component.ts
@@ -1,12 +1,59 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
+enum TimeFrame {
+  year = "TIMEFRAME_YEAR",
+  month = "TIMEFRAME_MONTH",
+  week = "TIMEFRAME_WEEK",
+  day = "TIMEFRAME_DAY",
+}
+
+const monthNames = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
+function createCalendarLabel(viewDate: Date, timeframe: TimeFrame): string {
+  switch(timeframe){
+  case TimeFrame.year:
+    return "" + viewDate.getFullYear();
+  case TimeFrame.month:
+    return monthNames[viewDate.getMonth()];
+  case TimeFrame.week:
+    return "Week Label Not Implemented";
+  case TimeFrame.day:
+    return "" + viewDate.getDay() + " " + monthNames[viewDate.getMonth()] + ", " + viewDate.getFullYear();
+  }
+}
+ 
 @Component({
   selector: 'app-event-schedule',
   standalone: true,
-  imports: [],
+  imports: [ CommonModule ],
   templateUrl: './event-schedule.component.html',
   styleUrl: './event-schedule.component.css'
 })
 export class EventScheduleComponent {
+ public viewDate: Date = new Date();
+ public editEvents: boolean = true;
+ public timeframe: TimeFrame = TimeFrame.month;
+ public calendarLabel: string = "Events for " + createCalendarLabel(this.viewDate, this.timeframe);
+
+ onChangeTimeFrame(event, newTimeFrame: string): void {
+  this.timeframe = newTimeFrame as TimeFrame;
+  this.calendarLabel = "Events for " + createCalendarLabel(this.viewDate, this.timeframe);
+ }
 
 }
+
+export { TimeFrame }
